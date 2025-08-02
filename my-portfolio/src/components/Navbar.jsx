ea2d9886ffd1027269880015a58e137efbe9e7c2
Aditya <adityakurhade1000@gmail.com>
2025-08-02 23:41:55 +0530

    nav update

--- my-portfolio/src/components/Navbar.jsx
+++ my-portfolio/src/components/Navbar.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav className="navbar">
-            <div className='container d-flex justify-content-between align-items-center'>
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/project">Project</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+        <nav className="navbar bg-white shadow-md py-4 m-8 rounded-lg w-4xl ml-auto mr-auto">
+            <div className="container flex justify-center items-center">
+                <ul className="flex gap-8 text-lg font-semibold justify-center items-center">
+                    <li>
+                        <div className='text-gray-400 hover:text-black'><Link to="/">Home</Link></div>
+                    </li>
+                    <li>
+                        <div className='text-gray-400 hover:text-black'><Link to="/about">About</Link></div>
+                    </li>
+                    <li>
+                        <div className='text-gray-400 hover:text-black'><Link to="/project">Project</Link></div>
+                    </li>
+                    <li>
+                        <div className='text-gray-400 hover:text-black'><Link to="/contact">Contact</Link></div>
+                    </li>
                 </ul>
             </div>
         </nav>
