ca92863c849be6a8210819b61b58abed298dfc07
Aditya <adityakurhade1000@gmail.com>
2025-08-03 01:18:33 +0530

    workExperience added

--- my-portfolio/src/pages/home.jsx
+++ my-portfolio/src/pages/home.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
+import WorkExperinceSection from '../components/WorkExperinceSection'
 
 const home = () => {
   return (
-    <div className="home-container">
+    <div className="home-container flex flex-row ">
       <div className="hero-section-container">
       <HeroSection />
+      </div>
+      <div>
+      <div className="work-experience-section-container">
+      <WorkExperinceSection />
+      </div>
       </div>
     </div>
   )
