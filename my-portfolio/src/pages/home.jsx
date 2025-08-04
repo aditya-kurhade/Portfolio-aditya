b4be1a9ed8e8f6ccfbfe2bdee25e109a41bd36d6
Aditya <adityakurhade1000@gmail.com>
2025-08-05 01:05:51 +0530

    home updated

--- my-portfolio/src/pages/home.jsx
+++ my-portfolio/src/pages/home.jsx
@@ -1,19 +1,31 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import WorkExperinceSection from '../components/WorkExperinceSection'
+import ExpertSection from '../components/ExpertSection'
+import ProjectSection from '../components/ProjectSection'
+
 
 const home = () => {
   return (
-    <div className="home-container flex flex-row ">
+    <>
+    <div className="home-container flex flex-row justify-center gap-10">
       <div className="hero-section-container">
       <HeroSection />
       </div>
       <div>
+        
       <div className="work-experience-section-container">
       <WorkExperinceSection />
       </div>
+      <div className='expert-section-container mt-7'>
+      <ExpertSection />
+      </div>
+      </div>
+      <div className="project-section-container">
+      <ProjectSection />
       </div>
     </div>
+    </>
   )
 }
 
