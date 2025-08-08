49e175d9a00787a409dc934ada5d8c4c385222b7
Aditya <adityakurhade1000@gmail.com>
2025-08-08 23:38:40 +0530

    contact stage 1 uncoded consist of timeline

---
+++ my-portfolio/src/pages/contact.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SkillTree from '../components/SkillTree'
+
+const contact = () => {
+  return (
+    <>
+    <div>contact</div>
+    <SkillTree />
+
+    </>
+  )
+}
+
+export default contact
