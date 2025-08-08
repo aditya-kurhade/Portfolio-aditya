9f6ecbc4544c7df7386c18932acc534953fb15c9
Aditya <adityakurhade1000@gmail.com>
2025-08-08 23:37:51 +0530

    app js update

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,8 +4,7 @@ import Navbar from './components/Navbar.jsx';
 import Home from './pages/home.jsx';
 import Project from './pages/project.jsx';
 import About from './pages/about.jsx';
-
-
+import Contact from './pages/contact.jsx';
 
 const App = () => {
     return (
@@ -16,6 +15,7 @@ const App = () => {
         background: "linear-gradient(135deg, #f0f4ff 0%, #ffffff 100%)"
       }}
     >
+        
             <BrowserRouter>
                 <div className="pt-10">
                 <Navbar />
@@ -24,6 +24,7 @@ const App = () => {
                     <Route exact path="/" element={<Home />} />
                     <Route path="/project" element={<Project />} />
                     <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
                 </Routes>
             
             </BrowserRouter>
