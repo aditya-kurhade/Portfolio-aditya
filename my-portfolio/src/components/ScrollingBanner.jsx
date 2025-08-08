be2a5f3357685cdb564981bda181be88a48d801d
Aditya <adityakurhade1000@gmail.com>
2025-08-08 23:36:52 +0530

    SCrolling Banner added

--- my-portfolio/src/components/ScrollingBanner.jsx
+++ my-portfolio/src/components/ScrollingBanner.jsx
@@ -4,8 +4,13 @@ const ScrollingBanner = () => {
   return (
     <div className="w-208 h-10 overflow-hidden bg-white text-black flex items-center justify-center p-6 shadow-md rounded-lg relative">
   <div className="whitespace-nowrap animate-marquee px-4 text-sm font-medium">
+    Creative Full Stack Developer |  Data-Driven Thinker | Proven Leadership as Peer Counsellor & ACM President | Delivering Impact through Code and Compassion
+  </div>
+  <div className="whitespace-nowrap animate-marquee px-4 text-sm font-medium">
     Creative Full Stack Developer | Data-Driven Thinker | Proven Leadership as Peer Counsellor & ACM President | Delivering Impact through Code and Compassion
   </div>
+
+  
 </div>
 
   )
