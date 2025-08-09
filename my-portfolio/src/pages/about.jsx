54d8a40bbd81b9d609b8848959f329d52ef32b60
Aditya <adityakurhade1000@gmail.com>
2025-08-10 01:34:59 +0530

    update

--- my-portfolio/src/pages/about.jsx
+++ my-portfolio/src/pages/about.jsx
@@ -55,6 +55,7 @@ const About = () => {
 
   return (
     <div className="about flex flex-col lg:flex-row justify-center gap-10 py-16 px-6 font-sans">
+      <h1>Delete after</h1>
 
       {/* Left Timeline */}
       {/* <div className="about-verticalTime w-full lg:w-2/5 mb-8 lg:mb-0 flex justify-center items-center ">
